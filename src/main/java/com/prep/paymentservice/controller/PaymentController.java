940ea56f49fc8a42f8355494a771425ab5073889
Komal Gaidhani Edu <komalgaidhani.edu@gmail.com>
2026-03-29 10:54:01 +0530

    Added Spingt boot Profiles and Context path for application

--- src/main/java/com/prep/paymentservice/controller/PaymentController.java
+++ src/main/java/com/prep/paymentservice/controller/PaymentController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(path = "/payments/api")
+@RequestMapping(path = "/api")
 public class PaymentController {
 
     @Autowired
